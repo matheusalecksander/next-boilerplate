be68e449a055fd9ff810e1cbd43df9125d652f11
Matheus Alecksander <matheus.ribeiro@lwtecnologia.com.br>
2024-07-23 11:42:51 -0300

    refactor: removing unused things

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,7 @@
 export function Main() {
   return (
-    <main>
-      <h1>React</h1>
+    <main className="flex items-center justify-center p-12 bg-slate-950">
+      <h1 className="text-3xl font-bold">React</h1>
     </main>
   )
 }
